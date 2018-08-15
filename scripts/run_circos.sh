ddb186c3da9c001fd81e9a464de8b4539032f938
Andres Espindola <espindo@ostatemail.okstate.edu>
2018-08-15 01:21:04 -0500

    nucmer

--- scripts/run_circos.sh
+++ scripts/run_circos.sh
@@ -7,7 +7,11 @@ genbank2fasta.pl sequences.gbk
 separatemultifasta.pl sequences.fasta
 # RUNNING NUCMER
 #------------------------------------------------------------------------------#
+for i in sequences.fasta.files/*.fasta
+do
+  echo $i
+  nucmer -maxmatch sequences.fasta.files/AY510453.fasta $i -p $i.nucmer
+done
 
-#nucmer -maxmatch ref.fasta qry.fasta -p output-name
 #delta-filter -q output-name.delta > output-name.filter.delta
 #show-coords output-name.filter.delta -T > output-name.coords
