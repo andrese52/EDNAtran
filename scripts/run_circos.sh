a12403e237a1ca5a7f17203b1515bab1df6f44a0
Andres Espindola <espindo@ostatemail.okstate.edu>
2018-08-15 00:45:30 -0500

    added mummer and separate multifasta to the circos run

--- scripts/run_circos.sh
+++ scripts/run_circos.sh
@@ -4,3 +4,10 @@
 
 get_genbank_batch.pl $1
 genbank2fasta.pl sequences.gbk
+separatemultifasta.pl sequences.fasta
+# RUNNING NUCMER
+#------------------------------------------------------------------------------#
+
+#nucmer -maxmatch ref.fasta qry.fasta -p output-name
+#delta-filter -q output-name.delta > output-name.filter.delta
+#show-coords output-name.filter.delta -T > output-name.coords
